08b446e8676620fb85c233df32f652556e7b6c20
Mateusz Kwapich <mateusz@kwapich.it>
2022-12-06 05:23:54 +0000

    Day 6: Tuning Trouble

---
+++ src/06a.rs
@@ -0,0 +1,23 @@
+// Advent of Code 2022
+// (c) 2002 Mateusz Kwapich
+
+use anyhow::Result;
+use std::collections::BTreeSet;
+use std::io;
+use std::io::Read;
+
+fn main() -> Result<()> {
+    let stdin = io::stdin();
+    let mut s = String::new();
+    stdin.lock().read_to_string(&mut s).expect("Couldn't read");
+    let s: Vec<_> = s.chars().collect();
+    for (i, w) in s[..].windows(4).enumerate() {
+        let mut temp_set = BTreeSet::new();
+        if w.iter().all(move |x| temp_set.insert(x)) {
+            println!("{}", i + 4);
+            break;
+        }
+    }
+
+    Ok(())
+}
